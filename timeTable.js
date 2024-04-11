231a6a90758460b7a141e7c6c4eef16260183485
Gaurav Goswami <142787056+itzgauravgoswami@users.noreply.github.com>
2024-04-11 12:46:46 +0530

    Add files via upload

---
+++ timeTable.js
@@ -0,0 +1,277 @@
+const Sunday =[
+    {   
+        time: 'Sunday',
+        roomNumber: 'Holiday',
+        subject: 'No class Available',
+        type: ''
+    }
+]
+const Monday =[
+    {   
+        time: '09:20 AM-10:10 AM',
+        roomNumber: 'S-102',
+        subject: 'Chemistry',
+        type: 'Lecture'
+    },
+    {   
+        time: '10:10 AM-11:00 AM',
+        roomNumber: 'S-102',
+        subject: 'WT',
+        type: 'Lecture'
+    },
+    {   
+        time: '11:00 AM-12:40 PM',
+        roomNumber: 'S-102',
+        subject: 'LANG LAB G1/ MP LAB G2/ CHEM LAB G3',
+        type: 'Tutorial'
+    },
+    {   
+        time: '12:40 PM-01:30 PM',
+        roomNumber: 'Break',
+        subject: 'Lunch',
+        type: 'Break'
+    },
+    {   
+        time: '01:30 PM-02:20 PM',
+        roomNumber: 'S-102',
+        subject: 'Chemistry',
+        type: 'Lecture'
+    },
+    {   
+        time: '02:20 PM-03:10 PM',
+        roomNumber: 'S-102',
+        subject: 'Math-II',
+        type: 'Lecture'
+    },
+    {   
+        time: '03:10 PM-04:00 PM',
+        roomNumber: 'S-102',
+        subject: 'Aptitude',
+        type: 'Lecture'
+    }
+]
+const Tuesday =[
+    {   
+        time: '09:20 AM-10:10 AM',
+        roomNumber: 'S-102',
+        subject: 'Aptitude',
+        type: 'Lecture'
+    },
+    {   
+        time: '10:10 AM-11:00 AM',
+        roomNumber: 'S-102',
+        subject: 'Chemistry',
+        type: 'Lecture'
+    },
+    {   
+        time: '11:00 AM-12:40 PM',
+        roomNumber: 'S-102',
+        subject: 'LANG LAB G3/ MP LAB G1/ CHEM LAB G2',
+        type: 'Tutorial'
+    },
+    {   
+        time: '12:40 PM-01:30 PM',
+        roomNumber: 'Break',
+        subject: 'Lunch',
+        type: 'Break'
+    },
+    {   
+        time: '01:30 PM-02:20 PM',
+        roomNumber: 'S-102',
+        subject: 'PPS',
+        type: 'Lecture'
+    },
+    {   
+        time: '02:20 PM-03:10 PM',
+        roomNumber: 'S-102',
+        subject: 'WT',
+        type: 'Lecture'
+    },
+    {   
+        time: '03:10 PM-04:00 PM',
+        roomNumber: 'S-102',
+        subject: 'Math-II',
+        type: 'Lecture'
+    }
+]
+
+const Wednesday =[
+    {   
+        time: '09:20 AM-10:10 AM',
+        roomNumber: 'S-102',
+        subject: 'PPS',
+        type: 'Lecture'
+    },
+    {   
+        time: '10:10 AM-11:00 AM',
+        roomNumber: 'S-102',
+        subject: 'Math-II',
+        type: 'Lecture'
+    },
+    {   
+        time: '11:00 AM-12:40 PM',
+        roomNumber: 'S-102',
+        subject: 'LANG LAB G2/ MP LAB G3/ CHEM LAB G1',
+        type: 'Tutorial'
+    },
+    {   
+        time: '12:40 PM-01:30 PM',
+        roomNumber: 'Break',
+        subject: 'Lunch',
+        type: 'Break'
+    },
+    {   
+        time: '01:30 PM-02:20 PM',
+        roomNumber: 'S-102',
+        subject: 'WT',
+        type: 'Lecture'
+    },
+    {   
+        time: '02:20 PM-03:10 PM',
+        roomNumber: 'S-102',
+        subject: 'Aptitude',
+        type: 'Lecture'
+    },
+    {   
+        time: '03:10 PM-04:00 PM',
+        roomNumber: 'S-102',
+        subject: 'Chemistry',
+        type: 'Lecture'
+    }
+]
+
+const Thursday =[
+    {   
+        time: '09:20 AM-10:10 AM',
+        roomNumber: 'S-102',
+        subject: 'Chemistry',
+        type: 'Lecture'
+    },
+    {   
+        time: '10:10 AM-11:00 AM',
+        roomNumber: 'S-102',
+        subject: 'WT',
+        type: 'Lecture'
+    },
+    {   
+        time: '11:00 AM-12:40 PM',
+        roomNumber: 'S-102',
+        subject: 'LANG LAB G1/ MP LAB G2/ CHEM LAB G3',
+        type: 'Tutorial'
+    },
+    {   
+        time: '12:40 PM-01:30 PM',
+        roomNumber: 'Break',
+        subject: 'Lunch',
+        type: 'Break'
+    },
+    {   
+        time: '01:30 PM-02:20 PM',
+        roomNumber: 'S-102',
+        subject: 'Chemistry',
+        type: 'Lecture'
+    },
+    {   
+        time: '02:20 PM-03:10 PM',
+        roomNumber: 'S-102',
+        subject: 'Math-II',
+        type: 'Lecture'
+    },
+    {   
+        time: '03:10 PM-04:00 PM',
+        roomNumber: 'S-102',
+        subject: 'Aptitude',
+        type: 'Lecture'
+    }
+]
+
+const Friday =[
+    {   
+        time: '09:20 AM-10:10 AM',
+        roomNumber: 'S-102',
+        subject: 'Aptitude',
+        type: 'Lecture'
+    },
+    {   
+        time: '10:10 AM-11:00 AM',
+        roomNumber: 'S-102',
+        subject: 'Chemistry',
+        type: 'Lecture'
+    },
+    {   
+        time: '11:00 AM-12:40 PM',
+        roomNumber: 'S-102',
+        subject: 'LANG LAB G3/ MP LAB G1/ CHEM LAB G2',
+        type: 'Tutorial'
+    },
+    {   
+        time: '12:40 PM-01:30 PM',
+        roomNumber: 'Break',
+        subject: 'Lunch',
+        type: 'Break'
+    },
+    {   
+        time: '01:30 PM-02:20 PM',
+        roomNumber: 'S-102',
+        subject: 'PPS',
+        type: 'Lecture'
+    },
+    {   
+        time: '02:20 PM-03:10 PM',
+        roomNumber: 'S-102',
+        subject: 'WT',
+        type: 'Lecture'
+    },
+    {   
+        time: '03:10 PM-04:00 PM',
+        roomNumber: 'S-102',
+        subject: 'Math-II',
+        type: 'Lecture'
+    }
+
+]
+
+const Saturday =[
+    {   
+        time: '09:20 AM-10:10 AM',
+        roomNumber: 'S-102',
+        subject: 'PPS',
+        type: 'Lecture'
+    },
+    {   
+        time: '10:10 AM-11:00 AM',
+        roomNumber: 'S-102',
+        subject: 'Math-II',
+        type: 'Lecture'
+    },
+    {   
+        time: '11:00 AM-12:40 PM',
+        roomNumber: 'S-102',
+        subject: 'LANG LAB G2/ MP LAB G3/ CHEM LAB G1',
+        type: 'Tutorial'
+    },
+    {   
+        time: '12:40 PM-01:30 PM',
+        roomNumber: 'Break',
+        subject: 'Lunch',
+        type: 'Break'
+    },
+    {   
+        time: '01:30 PM-02:20 PM',
+        roomNumber: 'S-102',
+        subject: 'WT',
+        type: 'Lecture'
+    },
+    {   
+        time: '02:20 PM-03:10 PM',
+        roomNumber: 'S-102',
+        subject: 'Aptitude',
+        type: 'Lecture'
+    },
+    {   
+        time: '03:10 PM-04:00 PM',
+        roomNumber: 'S-102',
+        subject: 'Chemistry',
+        type: 'Lecture'
+    }
+]
